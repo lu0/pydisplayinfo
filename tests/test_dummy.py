9b7501c947137ba0c4430298cf3c8076d80412cb
lu0 <me@lucerocodes.com>
2022-06-05 15:10:38 -0500

    Add dummy test cases for the dummy function

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,20 +1,33 @@
 import pytest
 
+from pydisplayinfo.dummy import dummy_sum
+
 
 class TestDummy:
     @pytest.mark.parametrize(
-        "a, b, c",
+        "a, b",
         [
-            ("one", "two", "three"),
-            ("1989", "13", "december"),
+            (-1, -3),
+            (-6, -7),
+            (-100, -1000),
         ],
     )
-    def test_dummy(self, a, b, c):
+    def test_sum_negatives_remain_negatives(self, a, b):
         """Dummy test"""
-        result = a + b + c
-        print(result)
-        assert type(a + b + c) == str
+        assert dummy_sum(a, b) < 0
 
-    def test_boolean(self):
-        """Most dummy test"""
-        assert True is True
+    @pytest.mark.parametrize(
+        "a, b",
+        [
+            (1, 3),
+            (6, 7),
+            (100, 1000),
+        ],
+    )
+    def test_sum_positives_remain_positives(self, a, b):
+        """Dummy test"""
+        assert dummy_sum(a, b) > 0
+
+    def test_sum_zeros(self):
+        """Another dummy test"""
+        assert dummy_sum(0, 0) == 0
