2aefb0f4d041fe561b9a069f039b02bb574dae22
lu0 <me@lucerocodes.com>
2022-06-08 01:52:05 -0500

    Implement DisplayInfo's properties
    - TODO: test on windows and wayland
    - TODO: Implement CLI properties
    - TODO: Fix test of expansion of the helper window
        - Or implement a mock one

--- pydisplayinfo/display_info.py
+++ pydisplayinfo/display_info.py
@@ -1,40 +1,56 @@
-from tkinter import Tk
 from typing import Dict, Tuple
+
+from pydisplayinfo import _HelperWindow
+
+DEFAULT_SIZE = (200, 200)
+DEFAULT_POSITION = (100, 100)
 
 
 class DisplayInfo:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, size=DEFAULT_SIZE, pos=DEFAULT_POSITION) -> None:
+        """Get information of the current display
+
+        Args:
+            size (Tuple[int, int]): Initial width and height of the helper window
+            pos (Tuple[int, int]): Initial x and y coordinates of the helper window
+        """
+        self.__window = _HelperWindow(*size, *pos)
 
     @property
-    def _window(self) -> Tk:
-        raise NotImplementedError
+    def _window(self) -> _HelperWindow:
+        return self.__window
 
     @property
     def resolution(self) -> Tuple[int, int]:
         """Resolution of the current display"""
-        raise NotImplementedError
+        self._window.expand_in_current()
+        width = self._window.winfo_width()
+        height = self._window.winfo_height()
+        return (width, height)
 
     @property
     def offset_x(self) -> int:
         """X coordinate of the top-left corner of the current display"""
-        raise NotImplementedError
+        self._window.expand_in_current()
+        return self._window.winfo_x()
 
     @property
     def offset_y(self) -> int:
         """Y coordinate of the top-left corner of the current display"""
-        raise NotImplementedError
+        self._window.expand_in_current()
+        return self._window.winfo_y()
 
     @property
     def width(self) -> int:
         """Width (resolution along the X axis) of the current display"""
-        raise NotImplementedError
+        self._window.expand_in_current()
+        return self._window.winfo_width()
 
     @property
     def height(self) -> int:
         """Height (resolution along the X axis) of the current display"""
-
-        raise NotImplementedError
+        self._window.expand_in_current()
+        return self._window.winfo_height()
 
     @classmethod
     def all(cls) -> Dict:
